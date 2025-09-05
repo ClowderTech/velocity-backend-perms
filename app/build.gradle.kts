b7f4673e1ac931050b28bbf50d6bd599e467f4e7
MrScarySpaceCat <mrcat@mrcat-desktop.local>
2025-09-04 22:32:22 -0400

    pkg: update some package versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,21 @@
 plugins {
     java
-    id("com.gradleup.shadow") version "9.0.0-beta15"
+    id("com.gradleup.shadow") version "9.1.0"
 }
 
 group = "com.clowdertech"
-version = "1.1.0"
+
+version = "1.1.3"
 
 repositories {
     mavenCentral()
     maven {
         name = "papermc"
-        url  = uri("https://repo.papermc.io/repository/maven-public/")
+        url = uri("https://repo.papermc.io/repository/maven-public/")
     }
     maven {
         name = "velocity-snapshots"
-        url  = uri("https://nexus.velocitypowered.com/repository/velocity-snapshots/")
+        url = uri("https://nexus.velocitypowered.com/repository/velocity-snapshots/")
     }
     maven {
         name = "mojang-libs"
@@ -25,21 +26,18 @@ repositories {
 dependencies {
     compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
     annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
-    compileOnly("net.luckperms:api:5.4")
+    compileOnly("net.luckperms:api:5.5")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
     compileOnly("com.mojang:brigadier:1.0.18")
     implementation("org.reflections:reflections:0.9.12")
 }
-
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
+tasks.withType<Test> { useJUnitPlatform() }
 
 tasks {
     // Configure the shadowJar task:
@@ -47,14 +45,10 @@ tasks {
         archiveBaseName.set("velocity-backend-perms")
         archiveVersion.set("")
         archiveClassifier.set("")
-        mergeServiceFiles()      // merges META‑INF/services if needed
+        mergeServiceFiles() // merges META‑INF/services if needed
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest {
-            attributes["Main-Class"] = "com.clowdertech.velocitybackendperms.Main"
-        }
+        manifest { attributes["Main-Class"] = "com.clowdertech.velocitybackendperms.Main" }
     }
 }
 
-tasks.named("build") {
-    dependsOn("shadowJar")
-}
+tasks.named("build") { dependsOn("shadowJar") }
